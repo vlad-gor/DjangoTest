141aad9d8bc59923e9324e2e8bd4101e1e1c33f9
Vladimir <gorunov341@yandex.ru>
2020-09-19 20:01:53 +0300

    form

--- Main/views.py
+++ Main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        age = request.POST.get("age")
+        
+    return render(request, 'index.html')
